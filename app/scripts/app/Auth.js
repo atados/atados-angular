ceae11e538d76711daad49225f3f2611bd02154d
Leonardo Arroyo <contato@leonardoarroyo.com>
2015-07-27 19:14:28 -0300

    Only show "Session expired" on invalid token

--- app/scripts/app/Auth.js
+++ app/scripts/app/Auth.js
@@ -109,20 +109,22 @@ app.factory('Auth', function($http, Cookies, Cleanup, api, accessTokenCookie, au
 app.factory('authInterceptorService', ['$q', '$window', '$injector', 'Cookies', function ($q, $window, $injector, Cookies){
   var responseError = function (rejection) {
     if (rejection.status === 403) {
-      var $http = $injector.get('$http');
+      if (rejection.data.detail == "Invalid token") {
+        var $http = $injector.get('$http');
 
-      Cookies.delete('access_token');
-      delete $http.defaults.headers.common.Authorization;
+        Cookies.delete('access_token');
+        delete $http.defaults.headers.common.Authorization;
 
-      var current_location = $window.location.pathname;
-      $window.location.href = '/#session-expired';
+        var current_location = $window.location.pathname;
+        $window.location.href = '/#session-expired';
 
-      // If the user is at the home, we need to force the reload
-      if (current_location === '/') {
-        $window.location.reload();
+        // If the user is at the home, we need to force the reload
+        if (current_location === '/') {
+          $window.location.reload();
+        }
+
+        return {};
       }
-
-      return {};
     }
     return $q.reject(rejection);
   };
