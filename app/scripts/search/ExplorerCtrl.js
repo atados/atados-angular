9c2022cc2aedea86610a7e37f91d4673f9cfa667
Leonardo Arroyo <contato@leonardoarroyo.com>
2015-08-25 15:18:38 -0300

    Fix hover on projects

--- app/scripts/search/ExplorerCtrl.js
+++ app/scripts/search/ExplorerCtrl.js
@@ -145,12 +145,14 @@ app.controller('ExplorerCtrl', function ($scope, $rootScope, $filter, Search,
       marker.setZIndex(100);
       $scope.previousMarker = marker;
     } else { // No marker
-      if (object.work.can_be_done_remotely || object.address.city.id === distancia.id) { // Trabalho a distancia
-        $scope.distanceAddress = true;
-        $('.map').css('opacity', 0.1);
-      } else if (object.address && (object.address.longitude === 0 || object.address.latitude === 0)) { // No address
-        $scope.hasAddress = true;
-        $('.map').css('opacity', 0.1);
+      if (object.work) {
+        if (object.work.can_be_done_remotely || object.address.city.id === distancia.id) { // Trabalho a distancia
+          $scope.distanceAddress = true;
+          $('.map').css('opacity', 0.1);
+        } else if (object.address && (object.address.longitude === 0 || object.address.latitude === 0)) { // No address
+          $scope.hasAddress = true;
+          $('.map').css('opacity', 0.1);
+        }
       }
     }
   };
