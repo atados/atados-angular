ee184d2f2862d90bd4b7aed4d401c55eb7780c77
Vinicius Pacheco <vin175pacheco@gmail.com>
2016-09-15 18:27:18 -0300

    Fixed About us Slide on small devices

--- app/scripts/cpanel/CpanelCtrl.js
+++ app/scripts/cpanel/CpanelCtrl.js
@@ -3,7 +3,18 @@
 var app = angular.module('atadosApp');
 
 app.controller('CpanelCtrl', function ($scope) {
-  $scope.site.title = 'Atados - Control Panel';;
+  $scope.site.title = 'Atados - Control Panel';
+
+  $scope.cpanel = {}
+
+  $scope.cpanel.notifications = [
+    {
+      type: 'alert',
+      text: 'Você possui 2 vagas pendentes para atualização!',
+      url: '/painel/vaga/1'
+    }
+  ];
+
 });
 
 
